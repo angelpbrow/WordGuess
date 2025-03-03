71eed5b6f40a1f7d7d1e2d8aa303600fadfb41ea
Angelika Brown <angelikpbrown@gmail.com>
2025-03-03 08:18:58 -0500

    Game begins

--- src/main/java/com/github/zipcodewilmington/Hangman.java
+++ src/main/java/com/github/zipcodewilmington/Hangman.java
@@ -1,10 +1,25 @@
 package com.github.zipcodewilmington;
-
+import java.sql.SQLOutput;
+import java.util.Scanner;
+import java.util.Random;
 
 /**
- * @author Angelika 
+ * @author Angelika
  * @version 1.0.0
  * @date 5/27/21 11:02 AM
  */
 public class Hangman {
+    public static void main(String[] args) {
+        String line;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Would you like to play a game?");
+        System.out.println("(Y/N)?");
+        System.out.print("Choose: ");
+        line = in.nextLine();
+        System.out.println();
+        System.out.println("````` Hangman Deluxe v 1.0 ````");
+
+    }
+
+
 }
